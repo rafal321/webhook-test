75789f2c7c3bd1ec6780191daeee2994ea438333
RafalK <rafal.kita@openjawtech.com>
2026-09-04 20:22:05 +0100

    maven app

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
